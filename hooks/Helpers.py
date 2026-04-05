43fe1dd87dd01e6b6104319b8020c45002639109
Fickel <fickelgam1@gmail.com>
2026-04-05 23:49:06 +0200

    every enabled operator can be included or excluded
    The operator options isn't working now. could be because manual version can't be determined?

--- hooks/Helpers.py
+++ hooks/Helpers.py
@@ -1,15 +1,27 @@
 from typing import Optional, Any
+# from ..Items import item_name_groups # need this for locations
 from BaseClasses import MultiWorld, Item, Location
 
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
 def before_is_category_enabled(multiworld: MultiWorld, player: int, category_name: str) -> Optional[bool]:
+    # uncomment and change only if some stages need certain characters (which should be never here)
+    # if category_name in item_name_groups["character"]:
+    #     # This category is the name of a champion
+    #     from ..Helpers import get_option_value
+    #     enabled_operators = get_option_value(multiworld, player, "enabled_operators")
+    #     return category_name in enabled_operators
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the item, False to disable it, or None to use the default behavior
 def before_is_item_enabled(multiworld: MultiWorld, player: int, item:  dict[str, Any]) -> Optional[bool]:
+    # Remove unwanted champions from the item pool
+    if "character" in item["category"]:
+        from ..Helpers import get_option_value
+        enabled_operators = get_option_value(multiworld, player, "Enabled_operators")
+        return item["name"] in enabled_operators  # True if they're in the yaml, false if they're not
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
--- hooks/Options.py
+++ hooks/Options.py
@@ -1,9 +1,9 @@
 # Object classes from AP that represent different types of options that you can create
-from Options import Option, FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, NamedRange, OptionGroup, PerGameCommonOptions
+from Options import Option, OptionSet, FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, NamedRange, OptionGroup, PerGameCommonOptions
 # These helper methods allow you to determine if an option has been set, or what its value is, for any player in the multiworld
 from ..Helpers import is_option_enabled, get_option_value
 from typing import Type, Any
-
+from ..Items import item_name_groups
 
 ####################################################################
 # NOTE: At the time that options are created, Manual has no concept of the multiworld or its own world.
@@ -32,8 +32,43 @@ class TotalCharactersToWinWith(Range):
     range_end = 50
     default = 50
 
+class Enabled6Star(OptionSet):
+    """enabled 6 star in the manual. only works when 6 star are individual items"""  # Description of the yaml option in the template
+    display_name = "Enabled 6 Star Operators"           # Name of the option in the spoiler
+    valid_keys = item_name_groups["6 star"]    # This is the bit that matters.  Our yaml option wants you to pick names of items in the Champion category
+    default = frozenset(valid_keys)              # This makes the default value list all of them.  It's easier for a player to delete ones they don't have than it is to guess what should be added.
+
+class Enabled5Star(OptionSet):
+    """enabled 5 star in the manual. only works when 5 star are individual items"""  # Description of the yaml option in the template
+    display_name = "Enabled 5 Star Operators"           # Name of the option in the spoiler
+    valid_keys = item_name_groups["5 star"]    # This is the bit that matters.  Our yaml option wants you to pick names of items in the Champion category
+    default = frozenset(valid_keys)              # This makes the default value list all of them.  It's easier for a player to delete ones they don't have than it is to guess what should be added.
+
+class Enabled4Star(OptionSet):
+    """enabled 4 star in the manual. only works when 4 star are individual items"""  # Description of the yaml option in the template
+    display_name = "Enabled 4 Star Operators"           # Name of the option in the spoiler
+    valid_keys = item_name_groups["4 star"]    # This is the bit that matters.  Our yaml option wants you to pick names of items in the Champion category
+    default = frozenset(valid_keys)              # This makes the default value list all of them.  It's easier for a player to delete ones they don't have than it is to guess what should be added.
+
+class Enabled3Star(OptionSet):
+    """enabled 3 star in the manual. only works when 3 star are individual items"""  # Description of the yaml option in the template
+    display_name = "Enabled 3 Star Operators"           # Name of the option in the spoiler
+    valid_keys = item_name_groups["3 star"]    # This is the bit that matters.  Our yaml option wants you to pick names of items in the Champion category
+    default = frozenset(valid_keys)              # This makes the default value list all of them.  It's easier for a player to delete ones they don't have than it is to guess what should be added.
+
+class EnabledLowStar(OptionSet):
+    """enabled 2 and 1 star in the manual. only works when 2 and 1 stars are individual items"""  # Description of the yaml option in the template
+    display_name = "Enabled low Star Operators"           # Name of the option in the spoiler
+    valid_keys = item_name_groups["low star"]    # This is the bit that matters.  Our yaml option wants you to pick names of items in the Champion category
+    default = frozenset(valid_keys)              # This makes the default value list all of them.  It's easier for a player to delete ones they don't have than it is to guess what should be added.
+
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict[str, Type[Option[Any]]]) -> dict[str, Type[Option[Any]]]:
+    options["exclude_6_star"] = Enabled6Star  # This registers the yaml option as `enabled_Operators`
+    options["exclude_5_star"] = Enabled5Star  # This registers the yaml option as `enabled_Operators`
+    options["exclude_4_star"] = Enabled4Star  # This registers the yaml option as `enabled_Operators`
+    options["exclude_3_star"] = Enabled3Star  # This registers the yaml option as `enabled_Operators`
+    options["exclude_low_star"] = EnabledLowStar  # This registers the yaml option as `enabled_Operators`
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
@@ -51,6 +86,8 @@ def after_options_defined(options: Type[PerGameCommonOptions]):
 # Use this Hook if you want to add your Option to an Option group (existing or not)
 def before_option_groups_created(groups: dict[str, list[Type[Option[Any]]]]) -> dict[str, list[Type[Option[Any]]]]:
     # Uses the format groups['GroupName'] = [TotalCharactersToWinWith]
+    groups["operator options"] = []
+    
     return groups
 
 def after_option_groups_created(groups: list[OptionGroup]) -> list[OptionGroup]:
