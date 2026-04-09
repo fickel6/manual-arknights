b3993e7d8fce71924cca6e1ffa8178fdec5a512e
Fickel <fickelgam1@gmail.com>
2026-04-10 00:15:31 +0200

    isn't getting removed correctly
    broken because its a different category than when I first tested it?

--- hooks/Helpers.py
+++ hooks/Helpers.py
@@ -19,8 +19,15 @@ def before_is_item_enabled(multiworld: MultiWorld, player: int, item:  dict[str,
     # Remove unwanted champions from the item pool
     if "character" in item["category"]:
         from ..Helpers import get_option_value
-        enabled_operators = get_option_value(multiworld, player, "Enabled_operators")
-        return item["name"] in enabled_operators  # True if they're in the yaml, false if they're not
+        enabled_6_star = get_option_value(multiworld, player, "whitelist_6_star")
+        enabled_5_star = get_option_value(multiworld, player, "whitelist_5_star")
+        enabled_4_star = get_option_value(multiworld, player, "whitelist_4_star")
+        enabled_3_star = get_option_value(multiworld, player, "whitelist_3_star")
+        enabled_low_star = get_option_value(multiworld, player, "whitelist_low_star")
+        if get_option_value(multiworld, player, "blacklist_or_whitelist_operators"):
+            return item["name"] in enabled_6_star or item["name"] in enabled_5_star or item["name"] in enabled_4_star or item["name"] in enabled_3_star or item["name"] in enabled_low_star  # True if they're in the yaml, false if they're not
+        else: 
+            return not (item["name"] in enabled_6_star or item["name"] in enabled_5_star or item["name"] in enabled_4_star or item["name"] in enabled_3_star or item["name"] in enabled_low_star)
     return None
 
 
